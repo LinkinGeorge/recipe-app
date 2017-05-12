052082726e0cd8c80f188024ff96d02454b389bf
LinkinGeorge <georgdonner98@gmail.com>
2017-05-12 21:35:16 +0100

    Configure deploy in src files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { Deploy } from '@ionic/cloud-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -13,12 +14,24 @@ export class MyApp {
   rootPage:any = HomePage;
   listPage:any = ShoppingListPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public deploy: Deploy) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+  }
+
+  ionViewDidLoad(){
+    this.deploy.check().then((snapshotAvailable: boolean) => {
+      if (snapshotAvailable) {
+        this.deploy.download().then(() => {
+          return this.deploy.extract().then(() => {
+            this.deploy.load();
+          });
+        });
+      }
     });
   }
 }
